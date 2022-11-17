c71ca1cf6292725dcd5ce35480bdc3ffac9f84b6
AlexandreS <virya.suy@gmail.com>
2022-11-17 01:57:43 +0100

    simplify TripRepository

--- src/main/java/com/loupsito/traga/infrastructure/database/TripRepository.java
+++ src/main/java/com/loupsito/traga/infrastructure/database/TripRepository.java
@@ -4,6 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface TripRepository extends JpaRepository<TripEntity, Integer> {
-    List<TripEntity> findAllByCreator(String userId);
+public interface TripRepository extends JpaRepository<TripEntity, Long> {
 }
