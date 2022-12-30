bceb8d3649a7ea51adebe373c4ad38462b2a18c9
AlexandreS <virya.suy@gmail.com>
2022-12-30 01:32:02 +0100

    fix: update swagger to use jwt to access protected endpoints

--- src/main/java/com/loupsito/traga/rest/TripController.java
+++ src/main/java/com/loupsito/traga/rest/TripController.java
@@ -4,6 +4,8 @@ import com.loupsito.traga.domain.dto.CreateTripDto;
 import com.loupsito.traga.domain.dto.UpdateTripDto;
 import com.loupsito.traga.infrastructure.database.TripEntity;
 import com.loupsito.traga.infrastructure.database.TripRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -16,27 +18,34 @@ import java.util.List;
 @RequestMapping("/api")
 public class TripController {
 
+    private final String BEARER_NAME = "Bearer Authentication";
     @Autowired
     TripRepository tripRepository;
 
+    @Operation(summary = "Get trips", description = "Get trips from a user")
+    @SecurityRequirement(name = BEARER_NAME)
     @GetMapping("/trips")
     public List<TripEntity> getAllTripsFromUser(@RequestParam(required = false) String userId) {
         return tripRepository.findAll();
     }
 
+    @Operation(summary = "Get one trip", description = "Get one trip by specifying its id")
+    @SecurityRequirement(name = BEARER_NAME)
     @GetMapping("/trips/{tripId}")
     public TripEntity getTrip(@PathVariable("tripId") long tripId) {
-        return tripRepository
-                .findById(tripId)
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Trip not found"));
+        return tripRepository.findById(tripId).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Trip not found"));
     }
 
+    @Operation(summary = "Create a trip", description = "Create a trip")
+    @SecurityRequirement(name = BEARER_NAME)
     @PostMapping("/trips")
     public void createTrip(@RequestBody CreateTripDto createTripDto) {
         TripEntity entity = new TripEntity(createTripDto.name(), createTripDto.creator(), LocalDateTime.now());
         tripRepository.save(entity);
     }
 
+    @Operation(summary = "Update a trip", description = "Update a trip")
+    @SecurityRequirement(name = BEARER_NAME)
     @PutMapping("/trips")
     public void updateTrip(@RequestBody UpdateTripDto updateTripDto) {
         TripEntity tripEntity = getTrip(updateTripDto.tripId());
@@ -45,8 +54,10 @@ public class TripController {
         tripRepository.save(tripEntity);
     }
 
+    @Operation(summary = "Delete a trip", description = "Delete a trip")
+    @SecurityRequirement(name = BEARER_NAME)
     @DeleteMapping("/trips/{tripId}")
-    public void deleteTrip (@PathVariable("tripId") long tripId) {
+    public void deleteTrip(@PathVariable("tripId") long tripId) {
         TripEntity tripEntity = getTrip(tripId);
         tripRepository.delete(tripEntity);
     }
